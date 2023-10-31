6e8347cf97ae771fd5e97a7449267625c7f6f524
Takahiro Egawa <2111140085a@ed.fuk.kindai.ac.jp>
2023-10-31 06:24:40 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  #resources :users
+  resources :users, except: :show
+  root 'users#index'
+  
+  get 'users/index'
+  get 'users/new'
+  post 'users/create'
+  delete 'users/:id', to: 'users#destroy'
+  
+  
+  #get 'users/:id', to: 'users#show', as: :users
+  
+
+  
+  #get 'users/:id', to: 'users#show', as: :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
